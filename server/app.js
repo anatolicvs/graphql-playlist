8dd10e99a98543ae27376f3164e0200a6095646e
Aytaç Ozkan <aytac@linux.com>
2019-01-02 05:38:24 +0100

    initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require("./schema/schema");
+const mongoose = require("mongoose")
+const app = express();
+
+
+mongoose.connect("mongodb://ozkan:Ao146544@ds145194.mlab.com:45194/gql-ninja", { useNewUrlParser: true });
+
+mongoose.connection.once('open', () => {
+    console.log("connected to database.");
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log("listening port:4000");
+});
+
